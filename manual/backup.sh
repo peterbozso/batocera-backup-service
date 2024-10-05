c989728b3281ce7333ee31776d0a2da184f3e49f
Péter Bozsó <peter.bozso@outlook.com>
2024-10-05 11:43:27 +0200

    Fix flatpak excluding

--- manual/backup.sh
+++ manual/backup.sh
@@ -4,10 +4,10 @@
 rclone sync /userdata/system/batocera.conf backup:/Games/Batocera/system
 
 echo "> SAVES sync" -------------------------------------------------------------------------------------
-rclone sync /userdata/saves backup:/Games/Batocera/saves -P --exclude flatpak
+rclone sync /userdata/saves backup:/Games/Batocera/saves -P --exclude "flatpak/**"
 
 echo "> ROMS sync" --------------------------------------------------------------------------------------
-rclone sync /userdata/roms backup:/Games/Batocera/roms -P --exclude flatpak
+rclone sync /userdata/roms backup:/Games/Batocera/roms -P --exclude "flatpak/**"
 
 echo "> BIOS sync" --------------------------------------------------------------------------------------
 rclone sync /userdata/bios backup:/Games/Batocera/bios -P
