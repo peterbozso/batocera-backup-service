ac39650c033e564e379b0d0a4aaca780158c11eb
Péter Bozsó <peter.bozso@outlook.com>
2024-10-05 11:25:29 +0200

    Fix remote name

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Backup script for Batocera v40 using rclone - https://batocera.org/changelog
 
-rclone sync /userdata/system/batocera.conf onedrive:/Games/Batocera/system
+rclone sync /userdata/system/batocera.conf backup:/Games/Batocera/system
 
 echo "> SAVES sync" -------------------------------------------------------------------------------------
-rclone sync /userdata/saves onedrive:/Games/Batocera/saves -P --exclude flatpak
+rclone sync /userdata/saves backup:/Games/Batocera/saves -P --exclude flatpak
 
 echo "> ROMS sync" --------------------------------------------------------------------------------------
-rclone sync /userdata/roms onedrive:/Games/Batocera/roms -P --exclude flatpak
+rclone sync /userdata/roms backup:/Games/Batocera/roms -P --exclude flatpak
 
 echo "> BIOS sync" --------------------------------------------------------------------------------------
-rclone sync /userdata/bios onedrive:/Games/Batocera/bios -P
+rclone sync /userdata/bios backup:/Games/Batocera/bios -P
